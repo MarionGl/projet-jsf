8a01e6b0a4d77d305e0e88203567bf0ec4b13a53
MarionGl <marion.gloriant@gmail.com>
2020-10-12 15:10:14 +0200

    ajout classe Reservation et Voyageur

---
+++ src/com/inti/entities/Reservation.java
@@ -0,0 +1,88 @@
+package com.inti.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Reservation implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idReservation;
+	@Temporal(TemporalType.DATE)
+	private Date dateReservation;
+	private int nbJours;
+	@ManyToOne
+	@JoinColumn(name="idVoyageur")
+	private Voyageur voyageur;
+	@ManyToOne
+	@JoinColumn(name="idHotel")
+	private Hotel hotel;
+	
+	public Reservation() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Reservation(Date dateReservation, int nbJours) {
+		super();
+		this.dateReservation = dateReservation;
+		this.nbJours = nbJours;
+	}
+
+	public Long getIdReservation() {
+		return idReservation;
+	}
+
+	public void setIdReservation(Long idReservation) {
+		this.idReservation = idReservation;
+	}
+
+	public Date getDateReservation() {
+		return dateReservation;
+	}
+
+	public void setDateReservation(Date dateReservation) {
+		this.dateReservation = dateReservation;
+	}
+
+	public int getNbJours() {
+		return nbJours;
+	}
+
+	public void setNbJours(int nbJours) {
+		this.nbJours = nbJours;
+	}
+	
+
+	public Voyageur getVoyageur() {
+		return voyageur;
+	}
+
+	public void setVoyageur(Voyageur voyageur) {
+		this.voyageur = voyageur;
+	}
+
+	@Override
+	public String toString() {
+		return "Reservation [dateReservation=" + dateReservation + ", nbJours=" + nbJours + "]";
+	}
+	
+	 
+	
+	
+	
+	
+
+}
